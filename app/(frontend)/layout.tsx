af53448ec807d2506617bdd89d985b3949c014ca
GraviQ Studios <277254275+graviqstudios@users.noreply.github.com>
2026-04-24 19:22:39 +0530

    Add WhatsApp chat button to layout

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -241,6 +241,7 @@ function WhatsAppButton() {
       className="fixed bottom-6 right-6 z-50 hover:scale-110 transition-all duration-300"
       aria-label="Chat on WhatsApp"
     >
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 48 48"
